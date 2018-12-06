cfe806c77abc47180413f8949d52fc2f72258758
Miro Furtado <furtadomiro@gmail.com>
2018-12-06 09:58:43 -0500

    Back to old pickle

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import bluebonnet as bb
 
 #Load in the data
-states=bb.load('data/all_states_prot2')
+states=bb.load('data/all_states_final.pickle')
 
 
 app = Flask(__name__)
